477404f9164caec6f645ffc92c474ff916073607
yadlashivaramaraju <yadlashivaramaraju@gamil.comm>
2026-03-19 23:09:27 +0530

    Removed Spring Security and fixed controller

--- src/main/java/com/hydravision/controller/BookingController.java
+++ src/main/java/com/hydravision/controller/BookingController.java
@@ -1,14 +1,19 @@
 package com.hydravision.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.hydravision.entity.Booking;
 import com.hydravision.entity.Screen;
 import com.hydravision.entity.User;
 import com.hydravision.repository.BookingRepository;
 import com.hydravision.service.FileStorageService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/bookings")
