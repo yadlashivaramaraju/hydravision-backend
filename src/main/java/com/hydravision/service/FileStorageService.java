7aac8e41b4554ee7bbff233ddf0cd2f44e073142
yadlashivaramaraju <yadlashivaramaraju@gamil.comm>
2026-03-24 12:14:26 +0530

    Forced absolute paths in FileStorageService

--- src/main/java/com/hydravision/service/FileStorageService.java
+++ src/main/java/com/hydravision/service/FileStorageService.java
@@ -14,20 +14,19 @@ import java.util.UUID;
 @Service
 public class FileStorageService {
 
-    private final String UPLOAD_DIR = "uploads/";
-
     public String saveImageLocally(MultipartFile file) throws IOException {
-        // 1. Create the "uploads" folder if it doesn't exist
-        File directory = new File(UPLOAD_DIR);
+        // 1. THE LINUX FIX: Force Java to calculate the exact Absolute Path
+        File directory = new File("uploads");
         if (!directory.exists()) {
             directory.mkdirs();
         }
+        String absolutePath = directory.getAbsolutePath() + "/";
 
         // 2. Generate a random name so files don't overwrite each other
         String uniqueFileName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
 
-        // 3. Save the file to your laptop
-        Path targetLocation = Paths.get(UPLOAD_DIR + uniqueFileName);
+        // 3. Save the file to those exact absolute coordinates
+        Path targetLocation = Paths.get(absolutePath + uniqueFileName);
         Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
 
         // 4. Return the file name to save in MySQL
